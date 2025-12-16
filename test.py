01d77515b482ff9f62ccffa0fb7ff647ac509353
Nicolas Beaujoin <nicolasbeaujoin@MacBook-Air-de-Nicolas.local>
2025-12-16 14:12:17 +0100

    test of new model

--- test.py
+++ test.py
@@ -4,27 +4,34 @@ from tqdm import tqdm
 from torch_geometric.loader import DataLoader
 
 from data_utils import PreprocessedGraphDataset  # Provided in challenge files
-from graph2caption import Graph2Caption
+# from graph2caption import Graph2Caption
+from train_all_pretrain_gpt import Graph2CaptionV2, MolGNN
 
 # 1. Setup
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("Device:", device)
+
+mol_enc = MolGNN(hidden=128).to(device)
+state_dict1 = torch.load("checkpoints/mol_enc.pt", map_location=device)
+mol_enc.load_state_dict(state_dict1)
+mol_enc.to(device)
 
 # Instantiate the model with the same configuration used during training
-model = Graph2Caption(gpt2_model_name="gpt2")
+model = Graph2CaptionV2(pretrained_encoder=mol_enc, gpt2_model_name="gpt2")
 
 # Ensure padding token is set (as in training)
 model.tokenizer.pad_token = model.tokenizer.eos_token
 
 # Load trained weights (state_dict) and move to device
-state_dict = torch.load("checkpoints/checkpoint_epoch_5.pt", map_location=device)
-model.load_state_dict(state_dict)
+state_dict2 = torch.load("checkpoints/g2cap_epoch_2.pt", map_location=device)
+model.load_state_dict(state_dict2)
 model.to(device)
 model.eval()  # Set to evaluation mode (turns off dropout, etc.)
 
 # 2. Load Test Data
 # Note: The document states test_graphs.pkl has no descriptions [cite: 23]
 test_dataset = PreprocessedGraphDataset("data/test_graphs.pkl")
-test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
+test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
 
 predictions = []
 
@@ -51,7 +58,7 @@ with torch.no_grad():
 # 4. Create Submission File
 # Format must be a CSV with "ID" and "description" columns
 df = pd.DataFrame(predictions)
-df.to_csv("submission.csv", index=False)
+df.to_csv("submission2.csv", index=False)
 
 print(f"Success! Generated predictions for {len(df)} molecules.")
 print("Upload 'submission.csv' to Kaggle to get your BLEU/BERTScore.")
